50ba87f7d28f20f0802f934000835951136bb10d
yulia tikhonova <yuliatikhonova@outlook.com>
2020-06-01 16:19:45 -0700

    wrote the init and writeToFile functions

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const axios = require("axios");
-const util = require("util");
+const util = require("./utils/generateMarkdown");
 
 
 const questions = [
     {
         type: "input",
         name: "name",
-        message: "What is your Github username?"
+        message: "What is your Github username?",
+        default: "yuliatikhonova"
     },
     {
         type: "input",
         name: "email",
-        message: "What is your email?"
+        message: "What is your email?",
+        default: "yuliatikhonova@outlook.com"
     },
     {
         type: "input",
         name: "URL",
-        message: "What is the URL for your project?"
+        message: "What is the URL for your project?",
     },
     {
         type: "input",
-        name: "tittle",
+        name: "title",
         message: "What is the projects title?"
     },
     {
@@ -31,16 +32,17 @@ const questions = [
         message: "Please write a short description."
     },
     {
-        type: "input",
+        type: "list",
         name: "License",
-        message: "What license dose your project have?"
+        message: "What license dose your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3.0", "None"]
     },
     {
         type: "input",
         name: "Installation",
         message: "What commands to run to install dependencies?"
     },
-    
+
     {
         type: "input",
         name: "Tests",
@@ -59,10 +61,17 @@ const questions = [
 ];
 
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) { return console.log(err); }
+        console.log("You got it Girl!");
+    });
+
 }
 
 function init() {
-
+    inquirer.prompt(questions).then(answers => {
+        writeToFile("README.md", util(answers));
+    })
 }
 
 init();
