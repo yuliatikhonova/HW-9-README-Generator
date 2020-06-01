9ca55b36e1d544135888d1d4161e1ed9c72cdf60
yulia tikhonova <yuliatikhonova@outlook.com>
2020-06-01 16:51:58 -0700

    formatted most of the readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,41 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+## Description
+
+${data.Description}
+
+## Table of Contents
+
+
+
+## Installation
+
+${data.Installation} 
+
+## Usage
+
+${data.Usage} 
+
+## License
+
+${data.License} 
+
+## Contributing
+
+${data.Contributing} 
+
+## Tests
+
+${data.Tests} 
+
+## Questions
+
+${data.Contributing} 
+
+## GIF
+
+
 `;
 }
 
